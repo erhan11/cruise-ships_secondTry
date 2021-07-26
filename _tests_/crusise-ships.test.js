30cbfbf3ae6a69f9de8a56bad0bac436bfb15519
Erhan11 <erhan0@hotmail.com>
2021-07-26 12:35:35 +0300

    it has a port test completed

--- _tests_/crusise-ships.test.js
+++ _tests_/crusise-ships.test.js
@@ -1,10 +1,17 @@
 const Ship = require('../src/ship');
 
 describe('constructor', () => {
+    let ship;
+beforeEach(() => {
+    ship = new Ship;
+});
+
 it('can be instantiated', () => {
     expect (new Ship).toBeInstanceOf(Object);
 });
-
+it('has a starting port', () => {
+    expect (ship.startingPort).toBe('');
+})
 
 
 });
