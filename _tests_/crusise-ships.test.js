be40cb86ce782effe59a703ab3a807b840d4650a
Erhan11 <erhan0@hotmail.com>
2021-07-26 12:48:04 +0300

    StartingPort = Dover

--- _tests_/crusise-ships.test.js
+++ _tests_/crusise-ships.test.js
@@ -1,17 +1,19 @@
-const Ship = require('../src/ship');
+const Ship = require("../src/ship");
 
-describe('constructor', () => {
-    let ship;
-beforeEach(() => {
-    ship = new Ship;
-});
+describe("constructor", () => {
+  let ship;
+  beforeEach(() => {
+    ship = new Ship();
 
-it('can be instantiated', () => {
-    expect (new Ship).toBeInstanceOf(Object);
-});
-it('has a starting port', () => {
-    expect (ship.startingPort).toBe('');
-})
+  });
+
+  it("can be instantiated", () => {
+    expect(new Ship()).toBeInstanceOf(Object);
+  });
 
 
+  it("has a starting port", () => {
+    ship.startingPort = 'Dover'
+    expect(ship.startingPort).toBe('Dover');
+  });
 });
